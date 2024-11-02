ea4d8c7ed9872b786d954779f0e1d7af2b7b03ca
Brandon Scott <brandonscott@gmail.com>
2024-11-01 22:33:07 -0700

    Update SportsCategory.vue

--- src/components/SportsCategory.vue
+++ src/components/SportsCategory.vue
@@ -1,25 +1,40 @@
 <template>
-    <div>
-    <h3>{{ categoryName === 'Mens Varsity' ? "Men's Varsity" : categoryName === 'Women Varsity' ? "Women's Varsity" : categoryName }}</h3>
-      <ul>
-        <li class="sport-listing" v-for="(sport, sportName) in sports" :key="sportName">
-          <h4>{{ sportName }}</h4>
-          <div class="division" v-if="category !== 'Club' && category !== 'Intramural'">
-            <span>{{ sport.Division }}</span>
-            <span class="subdivision" v-if="sport.Subdivision.trim() !== ''">{{ sport.Subdivision }}</span>
-          </div>
-          <div v-else>
-            <span> - <span class="gender">{{ sport.Gender }}</span></span>
-          </div>
-        </li>
-      </ul>
-    </div>
-  </template>
+	<div>
+		<h3>{{ categoryName === 'Mens Varsity' ? "Men's Varsity" : categoryName === 'Women Varsity' ? "Women's Varsity" : categoryName }}</h3>
+		<ul>
+			<li class="sport-listing flex-column" v-for="(sport, sportName) in sports" :key="sportName">
+				<div class="d-flex ga-4">
+					<h4>{{ sportName }}</h4>
+					<div class="division" v-if="category !== 'Club' && category !== 'Intramural'">
+							<span>{{ sport.Division }}</span>
+							<span class="subdivision" v-if="sport.Subdivision.trim() !== ''">{{ sport.Subdivision }}</span>
+					</div>
+					<div v-else>
+							<span> - <span class="gender">{{ sport.Gender }}</span></span>
+					</div>
+				</div>
+				<div class="d-flex ga-4 mb-8">
+					<v-btn @click="editSport(sportName)">Edit</v-btn>
+					<v-btn @click="deleteSport(sportName)">Delete</v-btn>
+				</div>
+			</li>
+		</ul>
+	</div>
+</template>
   
-  <script>
+<script>
+	import { useUserStore } from '../stores/userStore';
   export default {
     name: 'SportsCategory',
-    props: {
+		setup() {
+			let userStore = useUserStore();
+			userStore.getAdminMode();
+
+			return {
+				userStore,      
+			};    
+		},
+		props: {
       categoryName: {
         type: String,
         required: true
@@ -32,6 +47,11 @@
         type: String,
         required: true
       }
-    }
+    },
+		methods: {
+			deleteSport(sportName) {
+				console.log(sportName);
+			}
+		}
   }
-  </script>
+</script>
